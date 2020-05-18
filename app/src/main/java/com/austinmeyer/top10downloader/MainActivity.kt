0ad8b925e804e552a38b2b17d78eeaec2239094f
Austin Meyer <Austin.a.Meyer@gmail.com>
2020-05-18 18:59:08 -0500

    having a working RSS feed check

--- app/src/main/java/com/austinmeyer/top10downloader/MainActivity.kt
+++ app/src/main/java/com/austinmeyer/top10downloader/MainActivity.kt
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import java.io.BufferedReader
+import java.io.IOException
 import java.io.InputStreamReader
+import java.lang.Exception
 import java.lang.StringBuilder
 import java.net.HttpURLConnection
+import java.net.MalformedURLException
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
@@ -18,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.d(TAG, "onCreate Called")
         val downloadData = DownloadData()
-        downloadData.execute("URL goes here")
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
         Log.d(TAG, "onCreate: done")
     }
 
@@ -40,22 +43,46 @@ class MainActivity : AppCompatActivity() {
                     Log.e(TAG, "doInBackground: Error downloading")
                 }
                 return rssFeed
+            }
+            private fun downloadXML(urlPath: String?): String {
+                val xmlResult = StringBuilder()
 
+                try {
+                    val url = URL(urlPath)
+                    val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+                    val response = connection.responseCode
+                    Log.d(TAG, "downloadXML: The response code was $response")
+//            val inputStream = connection.inputStream
+//            val inputStreamReader = InputStreamReader(inputStream)
+//            val reader = BufferedReader(inputStreamReader)
+
+                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
+
+                    val inputBuffer = CharArray(500)
+                    var charsRead = 0
+                    while (charsRead >= 0) {
+                        charsRead = reader.read(inputBuffer)
+                        if (charsRead > 0) {
+                            xmlResult.append(String(inputBuffer,0,charsRead))
+                        }
+                    }
+                    reader.close()
+                    Log.d(TAG, "Recieved ${xmlResult.length} bytes")
+                    return xmlResult.toString()
+
+                } catch (e: MalformedURLException) {
+                    Log.e(TAG, "downloadXML: Invalid URL ${e.message}")
+                } catch (e: IOException) {
+                    Log.e(TAG, "downloadXML: IO Exception reading data: ${e.message}")
+                } catch (e: SecurityException) {
+                    e.printStackTrace()
+                    Log.e(TAG, "downloadXML: Security exception. Needs permissions? ${e.message}")
+                } catch (e: Exception) {
+                    Log.e(TAG, "Unkown error: ${e.message}")
+                }
+                return "" // If it gets to here, there has been a pbromgle. Return and empty string
             }
         }
     }
-    private fun downloadXML(urlPath: String?): String {
-        val xmlResult = StringBuilder()
 
-        try {
-            val url = URL(urlPath)
-            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-            val response = connection.responseCode
-            Log.d(TAG, "downloadXML: The response code was $response")
-            val inputStream = connection.inputStream
-            val inputStreamReader = InputStreamReader(inputStream)
-            val reader = BufferedReader(inputStreamReader)
-
-        }
-    }
 }
